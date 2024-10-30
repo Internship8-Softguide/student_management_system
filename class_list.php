f39dd42689cb9c39960afd06972ca568117b8209
Saw Theodore <sawtheodore@gmail.com>
2024-10-30 12:03:14 +0630

    update and delete

--- class_list.php
+++ class_list.php
@@ -1,4 +1,12 @@
 <?php require_once ("./layout/header.php") ?>
+<?php
+if (isset($_GET["delete_id"])) {
+    $class_id = $_GET["delete_id"];
+    if (!delete_class($mysqli, $class_id)) {
+        echo "can not delete class";
+    }
+}
+?>
 <h2>Class List</h2>
 <div class="card">
     <div class="card-header d-flex justify-content-end">
@@ -11,6 +19,7 @@
                     <th>NO</th>
                     <th>Name</th>
                     <th>Description</th>
+                    <th>Action</th>
                 </tr>
             </thead>
             <tbody>
@@ -26,6 +35,10 @@
                             echo "...";
                         } ?>
                     </td>
+                    <td>
+                        <a class="btn btn-sm btn-primary" href="./add_class.php?class_id=<?= $class["class_id"] ?>"><i class="bi bi-pen"></i></a>
+                        <button class="btn btn-sm btn-danger ms-2 confirmDelete" data-id="<?= $class["class_id"] ?>" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="bi bi-trash"></i></button>
+                    </td>
                 </tr>                  
                 <?php $i++;
                 } ?>  
@@ -33,4 +46,36 @@
         </table>
     </div>
 </div>
+<!-- Button trigger modal -->
+
+<!-- Modal -->
+<div class="modal fade modal-sm" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Are you sure!</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        Do you want to delete this class?
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" id="deleted">Delete</button>
+      </div>
+    </div>
+  </div>
+</div>
+<script>
+    let deleteId =undefined;
+    let confirmBtn = document.querySelectorAll(".confirmDelete");
+    let deleted = document.querySelector("#deleted");
+    confirmBtn.forEach(element => {
+    element.addEventListener("click",()=>{
+          deleteId = element.getAttribute('data-id')
+    })
+});
+deleted.addEventListener("click",()=>{
+    location.replace("./class_list.php?delete_id="+deleteId);
+})
+</script>
 <?php require_once ("./layout/footer.php") ?>
--- stroage/class_crud.php
+++ stroage/class_crud.php
@@ -23,8 +23,7 @@ function get_class_id($mysqli, $class_id)
 function update_class($mysqli, $class_id, $class_name, $description)
 {
     $sql = "UPDATE  `class` SET `class_name`='$class_name', `description`='$description'  WHERE `class_id`=$class_id";
-    $resule = $mysqli->query($sql);
-    return $resule->fetch_assoc();
+    return $mysqli->query($sql);
 }
 
 function delete_class($mysqli, $class_id)
